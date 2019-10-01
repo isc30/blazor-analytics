93e9d5e3e417b4386447ea6d19807425e3f8d10d
isc30 <ivansanzcarasa@gmail.com>
2019-10-02 00:53:25 +0200

    Fix Build and Upgrade Nuget Package

--- src/Blazor.Analytics/GoogleAnalytics/Components/GoogleAnalyticsComponent.cs
+++ src/Blazor.Analytics/GoogleAnalytics/Components/GoogleAnalyticsComponent.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Blazor.Analytics.Constants;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.JSInterop;
 
 namespace Blazor.Analytics.GoogleAnalytics.Components
@@ -9,7 +10,7 @@ namespace Blazor.Analytics.GoogleAnalytics.Components
     public class GoogleAnalyticsComponent : ComponentBase, IDisposable
     {
         [Parameter]
-        protected string TrackingId { get; set; } = null;
+        public string TrackingId { get; set; } = null;
 
         [Inject]
         protected NavigationManager NavigationManager { get; set; } = null;
@@ -17,10 +18,11 @@ namespace Blazor.Analytics.GoogleAnalytics.Components
         [Inject]
         protected IJSRuntime JSRuntime { get; set; } = null;
 
-        protected override async Task OnInitAsync()
+        protected override async Task OnInitializedAsync()
         {
-            base.OnInit();
+            await base.OnInitializedAsync();
 
+            NavigationManager.LocationChanged -= OnLocationChanged;
             NavigationManager.LocationChanged += OnLocationChanged;
 
             await JSRuntime.InvokeAsync<string>(GoogleAnalyticsInterop.Configure,
@@ -32,9 +34,9 @@ namespace Blazor.Analytics.GoogleAnalytics.Components
             NavigationManager.LocationChanged -= OnLocationChanged;
         }
 
-        private async void OnLocationChanged(object sender, string absoluteUri)
+        private async void OnLocationChanged(object sender, LocationChangedEventArgs args)
         {
-            var relativeUri = new Uri(absoluteUri).PathAndQuery;
+            var relativeUri = new Uri(args.Location).PathAndQuery;
 
             await JSRuntime.InvokeAsync<string>(GoogleAnalyticsInterop.Navigate,
                 TrackingId, relativeUri);
