f02ad86d811cce85dcd7c9a659f56907703a2cd0
isc30 <ivansanzcarasa@gmail.com>
2020-01-11 20:02:59 +0100

    It works

--- demo/DemoApp/DemoApp.Server/Program.cs
+++ demo/DemoApp/DemoApp.Server/Program.cs
@@ -1,13 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace DemoApp.Server
 {
@@ -22,7 +14,6 @@ namespace DemoApp.Server
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStaticWebAssets();
                     webBuilder.UseStartup<Startup>();
                 });
     }
