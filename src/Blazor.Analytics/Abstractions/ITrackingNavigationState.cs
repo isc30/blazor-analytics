8febc9416c88d429064fb6d762b6da94d251597b
Tomás López <tomas.lopez.rod@gmail.com>
2021-07-29 15:09:00 +0200

    Added ability to disable tracking on certain pages (#38)
    Co-authored-by: Tomás López <tlopez@facephi.com>

---
+++ src/Blazor.Analytics/Abstractions/ITrackingNavigationState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.Analytics.Abstractions
+{
+    public interface ITrackingNavigationState
+    {
+        void EnableTracking();
+
+        void DisableTracking();
+
+        bool IsTrackingEnabled();
+    }
+}
---
+++ src/Blazor.Analytics/Components/TrackingNavigationState.cs
@@ -0,0 +1,18 @@
+﻿using Blazor.Analytics.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.Analytics.GoogleAnalytics
+{
+    public class TrackingNavigationState : ITrackingNavigationState
+    {
+        private bool _isEnableTracking = true;
+
+        public void DisableTracking() => _isEnableTracking = false;
+
+        public void EnableTracking() => _isEnableTracking = true;
+
+        public bool IsTrackingEnabled() => _isEnableTracking;
+    }
+}
