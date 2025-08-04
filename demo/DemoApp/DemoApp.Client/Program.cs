b33850edad064b0921c08b0bb9ea0bec3c401197
Martin Alderson <martinalderson@gmail.com>
2025-08-04 14:45:19 +0100

    Fix build errors by modernizing DemoApp.Client to use .NET 8.0 and Blazor WebAssembly
    - Updated DemoApp.Client.csproj to use Microsoft.NET.Sdk.BlazorWebAssembly and .NET 8.0
    - Modernized Program.cs to use WebAssemblyHostBuilder pattern
    - Removed obsolete Startup.cs file
    - Updated GitVersion.yml configuration
    - Fixed DemoApp.Server target framework to net8.0

--- demo/DemoApp/DemoApp.Client/Program.cs
+++ demo/DemoApp/DemoApp.Client/Program.cs
@@ -1,16 +1,20 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Blazor.Analytics;
+using System.Threading.Tasks;
 
 namespace DemoApp.Client
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+            builder.Services.AddGoogleAnalytics("UA-111742878-2");
+
+            await builder.Build().RunAsync();
+        }
     }
 }
